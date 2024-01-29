5a0d17c784a9c3099fa925e319ada58f69cd7309
sdbuch <19997120+sdbuch@users.noreply.github.com>
2024-01-29 17:25:39 -0600

    initial

---
+++ data/square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# imports
+import dataclasses
+from collections.abc import Callable
+from pathlib import Path
+from typing import Dict, Literal, Optional, Tuple, cast
+
+import matplotlib.pyplot as plt
+import numpy.typing as npt
+import torch
+
+
