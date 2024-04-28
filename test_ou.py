faf28c8c61aeed02c61ca4a35bc8a14c994ca569
sdbuch <19997120+sdbuch@users.noreply.github.com>
2024-04-28 16:39:54 -0500

    experiment changes

--- test_ou.py
+++ test_ou.py
@@ -6,6 +6,7 @@ from math import exp, sqrt
 import matplotlib.pyplot as plt
 import torch
 import tyro
+from numpy.random import default_rng
 from tqdm import tqdm
 
 import wandb
@@ -17,13 +18,15 @@ from util.metrics import chamfer
 from util.visualization import plot_histogram
 
 
+# TODO: write a sweep?
 def test_ou():
     # etc
     device = torch.device("cuda")
 
     # Experiment params
     dims = range(10, 1000, 10)
-    num_samples = 1000
+    num_samples = 10000
+    downsample_factor = 2  # HACK: setting to 0 means no downsampling
 
     # sampler params
     min_time = 1e-4
@@ -38,6 +41,7 @@ def test_ou():
             "early_stopping_time": min_time,
             "discretization_length": num_points,
             "debias": debias,
+            "downsample_factor": downsample_factor
         },
         # | dataclasses.asdict(config),
     )
@@ -45,6 +49,9 @@ def test_ou():
     wandb.define_metric("distance", step_metric="dimension")
     wandb.define_metric("mean distance", step_metric="dimension")
     wandb.define_metric("normalized distance", step_metric="dimension")
+    wandb.define_metric("uniformness", step_metric="dimension")
+
+    rng = default_rng()
 
     # We try to estimate progress based on time complexity of the sampler
     # auxiliary progress bar...
@@ -54,10 +61,14 @@ def test_ou():
         for idx_dim in range(len(dims)):
             # target empirical measure
             dim = dims[idx_dim]
+            if downsample_factor == 0:
+                downsample = 1
+            else:
+                downsample = dim // downsample_factor
             data = TranslationalImageManifoldify(
                 base_dataset=StepDataset(dimension=dim, device=device),
                 device=device,
-                downsample_factor=dim // 2,
+                downsample_factor=downsample,
             )
             X = [sample[0][None, ...] for sample in data]
             X = torch.cat(X)
@@ -75,25 +86,35 @@ def test_ou():
                 )
             )  # TODO: seems not ideal that score function depends so much on the process (but maybe unavoidable? possibly interface should be different...)
             # sampler
+            seed = int(rng.integers(0, high=2**32))
             sampler = BasicOUSampler(
                 dimension=X.shape[1:],
                 score_estimator=score,
                 min_time=min_time,
                 num_points=num_points,
                 device=device,
+                seed=seed,
             )
             samples = sampler.sample(num_samples, debias=debias)
 
             # calculate ell^2 distances
             # It corresponds to an upper bound on W_1 for ell^2
             # Hence the normalization factor is 1/sqrt(dim)
-            distances = chamfer(X, samples)
+            distances, minimizer_args = chamfer(X, samples)
+            histogram = torch.tensor(
+                [minimizer_args.tolist().count(i) for i in range(dim // downsample)],
+                dtype=torch.float32,
+            )
+            uniformness = torch.exp(torch.mean(torch.log(histogram))) / torch.mean(
+                histogram
+            )
 
             wandb.log(
                 {
                     "dimension": dim,
                     "distance": distances,
                     "mean distance": torch.mean(distances),
+                    "uniformness": uniformness,
                     "normalized distance": distances / sqrt(dim),
                 }
             )
